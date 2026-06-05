36c260122c5e0cbee77c86c5baf0e55fad790644
Isaac Yanez <isaac231467@gmail.com>
2026-06-05 10:52:48 -0500

    testing the lambda functions still.

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -3,11 +3,6 @@ import { auth } from "./auth/resource";
 import { data } from "./data/resource";
 import { myFunction } from "./test-function/resource";
 
-/**
- * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
- */
 defineBackend({
-  auth,
-  data,
   myFunction,
 });
